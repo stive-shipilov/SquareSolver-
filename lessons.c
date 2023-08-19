0c273e83782cf9b6c763a96032fd8d9e5c7853ab
stive-shipilov <68929831+stive-shipilov@users.noreply.github.com>
2023-08-19 13:06:49 +0300

    Change-1

--- lessons.c
+++ lessons.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
+ 
+const double EPS = 1e-9;
 
-int main(void) 
-{
-  double a, b, c, x1, x2;
-  printf("Enter the coefficients A, B, C separated by commas - %lf,%lf,%lf\n", a,b,c);
-  scanf("%lf, %lf, %lf", &a,&b,&c);
-  double dis = b*b - 4*a*c;
-  if (dis>=0) 
-  {
-    x1 = (-b+sqrt(dis))/(2*a);
-    x2 = (-b-sqrt(dis))/(2*a);
+bool comp_eq(double a, double b) {
+  return fabs(a-b) < EPS;
+}
+
+bool comp_more(double a, double b) {
+  return a-b > EPS;
+}
+
+int main(void) {
+  double a = 0.0, b=0.0, c=0.0;
+  printf("Enter the coefficients A, B, C separated by commas\n");
+  if(scanf("%lf, %lf, %lf", &a,&b,&c) == 3) {
+    double dis = b*b - 4*a*c;
+    if (comp_more(dis,0.0) == true) {
+      double x1 = (-b+sqrt(dis))/(2*a);
+      double x2 = (-b-sqrt(dis))/(2*a);
+      printf("Solution of the equation --> X1 = %.2lf, X2 = %.2lf",x1,x2);
+      return 0;       
+    }
+    else if(comp_eq(dis,0.0) == true) {
+      double x = (-b)/(2*a);
+      printf("Solution of the equation --> X = %.2lf",x);
+      return 0;
+    }
+    else {
+      printf("This equation has no solution");
+      return 0;
+    }
   }
-  if(dis<0) printf("This equation has no solution");
-  else if(dis == 0) printf("Solution of the equation --> X = %.2lf",x1);
-  else printf("Solution of the equation --> X1 = %.2lf, X2 = %.2lf",x1,x2);
-}              
+  else {
+    printf("Inpur error");
+    return 0;
+  }
+} 
